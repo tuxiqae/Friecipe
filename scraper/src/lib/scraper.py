7cf93ad406b11d2e239bdfd5bbef34af28fd0c35
Sagi Sarussi <sagis@sagis.dev>
2021-05-05 01:35:57 +0300

    - Organize the code by using helper functions. - Start crawling contact scraping.

--- scraper/src/lib/scraper.py
+++ scraper/src/lib/scraper.py
@@ -4,10 +4,13 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException, ElementNotInteractableException, NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.expected_conditions import invisibility_of_element_located
+from selenium.webdriver.support.wait import WebDriverWait
 
 from .review import Review
 
@@ -16,17 +19,22 @@ def init_driver() -> WebDriver:
     options = FirefoxOptions()
     # options.add_argument("--headless")  # TODO: Remove or not
     remote_host = f'http://{environ["SELENIUM"]}:4444/wd/hub'
+
     return webdriver.Remote(command_executor=remote_host,
                             keep_alive=False,  # TODO: Remove
                             options=options,
                             desired_capabilities=DesiredCapabilities.FIREFOX)
 
 
-def scroll_down_page(driver: WebDriver) -> bool:
-    more_btn = driver.find_element_by_id("moreBtn")
+DRIVER = init_driver()
 
-    while driver.find_element_by_class_name("loading-indicator").is_displayed():
-        sleep(0.1)
+
+def wait_for_load() -> None:
+    WebDriverWait(DRIVER, 10).until(invisibility_of_element_located((By.CLASS_NAME, "loading-indicator")))
+
+
+def scroll_down_page() -> bool:
+    more_btn = DRIVER.find_element_by_id("moreBtn")
 
     while True:
         try:
@@ -36,44 +44,78 @@ def scroll_down_page(driver: WebDriver) -> bool:
         sleep(1)  # TODO: check if sleep time can be reduced
 
 
-def scrape_profile_reviews(driver: WebDriver, review_set: set, user_id: str) -> bool:
-    if not scroll_down_page(driver):
+def scrape_profile_reviews(review_set: set, profile_id: str) -> bool:
+    wait_for_load()
+
+    if not scroll_down_page():
         return False
 
-    if driver is None:
-        return False
+    for card in DRIVER.find_element_by_class_name("profile-wrapper").find_elements_by_class_name("profile-review-card"):
+        review_set.add(get_review_from_card(card, profile_id))
 
-    for card in driver.find_element_by_class_name("profile-wrapper").find_elements_by_class_name("profile-review-card"):
-        review_set.add(get_review_from_card(card, user_id))
-
-
-def scrape_contacts(driver: WebDriver, profile_queue: SimpleQueue):
-    pass
-    # TODO: Implement
+    return True
 
 
-def is_profile_empty(driver: WebDriver) -> bool:
-    pass
+def scrape_contacts(profile_queue: SimpleQueue, profile_id: str, peer_type: str):
+    DRIVER.get(f"https://www.allrecipes.com/cook/{profile_id}/{peer_type}/")
+    wait_for_load()
+    scroll_down_page()
+
+    for contact in DRIVER.find_elements(By.CLASS_NAME, "cook-tile"):
+        profile_queue.put(get_id_from_url(contact.find_element(By.TAG_NAME, "a").get_property("href")))
 
 
-def profile_scraper(url: str, driver: WebDriver, profile_queue: SimpleQueue, viewed_profiles: set, review_set) -> bool:
+def is_profile_empty() -> bool:
+    return DRIVER.find_element_by_class_name("empty-page-header").is_displayed()
+
+
+def is_valid_profile() -> bool:
     try:
-        driver.get(url)
+        DRIVER.find_element(By.CLASS_NAME, "error-page")
+    except NoSuchElementException:
+        return True
+
+    return False
+
+
+def load_page(url: str):
+    try:
+        DRIVER.get(url)
     except WebDriverException as err:
         print("Could not open", url, "error:", str(err))
 
-    if driver.find_element_by_class_name("empty-page-header").is_displayed():
+
+def is_known_profile(profile_id: str, viewed_profiles: set) -> bool:
+    old_len = len(viewed_profiles)
+    viewed_profiles.add(profile_id)
+
+    if old_len == len(viewed_profiles):  # Check if profile_id in set
+        return True
+    return False
+
+
+def get_id_from_url(url: str) -> str:
+    return url.split("/")[4]
+
+
+def profile_scraper(url: str,
+                    profiles: SimpleQueue,
+                    viewed_profiles: set,
+                    reviews: set,
+                    recipes: set) -> bool:
+    load_page(url)
+    profile_id = get_id_from_url(DRIVER.current_url)
+
+    if is_known_profile(profile_id, viewed_profiles) or not is_valid_profile():
         return False
 
-    user_id = driver.current_url.split("/")[4]
+    if not is_profile_empty():
+        scrape_profile_reviews(review_set=reviews, profile_id=profile_id)
 
-    if user_id in viewed_profiles:
-        return False  # TODO: Change?
-    else:
-        viewed_profiles.add(user_id)
+    scrape_contacts(profiles, profile_id, "followers")
+    scrape_contacts(profiles, profile_id, "following")
 
-    scrape_profile_reviews(driver=driver, review_set=review_set, user_id=user_id)
-    scrape_contacts(driver=driver, profile_queue=profile_queue)
+    print(f"Finished scraping profile: '{profile_id}'")
 
 
 def get_review_from_card(review: WebElement, user_id) -> Review:
--- scraper/src/main.py
+++ scraper/src/main.py
@@ -1,21 +1,32 @@
-from lib import init_driver, profile_scraper
+from time import sleep
+
+from lib import profile_scraper
 
 from queue import SimpleQueue
 
 
 def main():
-    driver = init_driver()
     url = r"https://www.allrecipes.com/cook/thiswifecooks/reviews/"
     url_empty = r"https://www.allrecipes.com/cook/22195726/reviews/"
+    url_404 = r"https://www.allrecipes.com/cook/12696989/reviews"
+    main_seed = r"https://www.allrecipes.com/cook/16007298/"
     profile_id_queue: SimpleQueue = SimpleQueue()
     viewed_profiles: set = set()
     review_set: set = set()
-    profile_scraper(url, driver, profile_id_queue, viewed_profiles, review_set)
-    profile_scraper(url_empty, driver, profile_id_queue, viewed_profiles, review_set)
+    recipe_set: set = set()
+    profile_scraper(url, profile_id_queue, viewed_profiles, review_set, recipe_set)
+    profile_scraper(url_empty, profile_id_queue, viewed_profiles, review_set, recipe_set)
+    profile_scraper(url_404, profile_id_queue, viewed_profiles, review_set, recipe_set)
 
+    print("Reviews:")
     print(review_set)
     print("*" * 60)
+    print("viewed_profiles:")
     print(viewed_profiles)
+    print("*" * 60)
+    print("Queued profiles:")
+    while not profile_id_queue.empty():
+        print(profile_id_queue.get_nowait())
 
 
 if __name__ == '__main__':
